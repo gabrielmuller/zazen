e82504b26f0f85d330c2c896e85620cee5b3ce15
Gabriel Müller <gabrielmullerw@gmail.com>
2019-04-09 12:34:44 -0300

    Developing algorithm

--- cpu.cpp
+++ cpu.cpp
@@ -5,8 +5,8 @@
 #include <new>
 #include <limits>
 
-#define WIDTH 128
-#define HEIGHT 128
+#define WIDTH 256
+#define HEIGHT 256
 
 unsigned char texture[WIDTH][HEIGHT][3];             
 int t = 0;
@@ -29,12 +29,12 @@ struct Leaf {
     Leaf() = default;
 
     Leaf(uint8_t r, uint8_t g, uint8_t b, uint8_t a) :
-            r(r), g(g), b(b), a(a) {}
+        r(r), g(g), b(b), a(a) {}
 
-     inline void set_color(unsigned char* pixel) const {
-         pixel[0] = r;
-         pixel[0] = g;
-         pixel[0] = b;
+    inline void set_color(unsigned char* pixel) const {
+        pixel[0] = r;
+        pixel[0] = g;
+        pixel[0] = b;
     }
 };
 struct Vector {
@@ -42,7 +42,7 @@ struct Vector {
     float x, y, z;
     Vector() = default;
     explicit Vector(float x, float y, float z) :
-            x(x), y(y), z(z) {}
+        x(x), y(y), z(z) {}
 
     Vector(const Vector& v) : Vector(v.x, v.y, v.z) {}
 
@@ -70,8 +70,8 @@ struct Ray {
     /* Simple container class for a ray. */
     Vector origin, direction, negdir;
     explicit Ray(const Vector origin, const Vector direction) :
-            origin(origin), direction(direction) {
-    }
+        origin(origin), direction(direction) {
+        }
 
     uint8_t octant_mask() {
         uint8_t mask = 0;
@@ -88,71 +88,69 @@ struct AABB {
     float size;
 
     explicit AABB(const Vector corner, const float size) : 
-            corner(corner), size(size) {}
+        corner(corner), size(size) {}
 
     inline bool contains(const Vector vector) const 
     {
         /* Check if vector is inside box */
         return vector.x >= corner.x &&
-               vector.y >= corner.y &&
-               vector.z >= corner.z && 
-               vector.x < corner.x + size &&
-               vector.y < corner.y + size && 
-               vector.z < corner.z + size;
+            vector.y >= corner.y &&
+            vector.z >= corner.z && 
+            vector.x < corner.x + size &&
+            vector.y < corner.y + size && 
+            vector.z < corner.z + size;
     }
 
     inline uint8_t get_octant
-            (const Vector vector) const {
-        /* Returns which octant the vector resides inside box. */
-        uint8_t octant = 0;
-        float oct_size = size * 0.5;
-        if (vector.x > corner.x + oct_size) octant ^= 4;
-        if (vector.y > corner.y + oct_size) octant ^= 2;
-        if (vector.z > corner.z + oct_size) octant ^= 1;
-        return octant;
-    }
+        (const Vector vector) const {
+            /* Returns which octant the vector resides inside box. */
+            uint8_t octant = 0;
+            float oct_size = size * 0.5;
+            if (vector.x > corner.x + oct_size) octant ^= 4;
+            if (vector.y > corner.y + oct_size) octant ^= 2;
+            if (vector.z > corner.z + oct_size) octant ^= 1;
+            return octant;
+        }
+};
+
+struct StackEntry {
+    Voxel* voxel;
+    uint8_t octant;
+    Vector corner;
 };
 
 struct VoxelStack {
-    Voxel** voxels;
-    uint8_t* octants;
+    StackEntry* entries;
     size_t top;
 
-     explicit VoxelStack(const size_t size) {
-        voxels = new Voxel*[size];
-        octants = new uint8_t[size];
+    explicit VoxelStack(const size_t size) {
+        entries = new StackEntry[size];
         top = 0;
     }
 
-     ~VoxelStack() {
-        delete[] voxels;
-        delete[] octants;
+    ~VoxelStack() {
+        delete[] entries;
     }
 
-     void push(Voxel* voxel, const uint8_t octant) {
-        voxels[top] = voxel;
-        octants[top] = octant;
+    void push(Voxel* voxel, uint8_t octant, Vector corner) {
+        entries[top] = {voxel, octant, corner};
         top++;
     }
 
-     Voxel* pop() {
+    StackEntry& pop() {
         /* call peek_oct before this if you need the octant position */
         top--;
-        return voxels[top];
+        return entries[top];
     }
 
-     inline Voxel* peek() {
-        return voxels[top - 1];
+    inline StackEntry& peek() {
+        return entries[top - 1];
     }
 
-     inline uint8_t& peek_oct() {
-        return octants[top - 1];
-    }
-
-     inline bool empty() {
+    inline bool empty() {
         return !top;
     }
-        
+
 };
 
 inline void printv(Vector vector) {
@@ -166,10 +164,10 @@ struct Block {
     char* front = nullptr;
 
     explicit Block(size_t element_count, char* data) :
-            element_count(element_count),
-            data(data) {
-        front = data;
-    }
+        element_count(element_count),
+        data(data) {
+            front = data;
+        }
 
     explicit Block(size_t element_count) : element_count(element_count) {
         data = new char[element_count * element_size];
@@ -186,9 +184,9 @@ struct Block {
     Block& operator=(Block&& rhs) = delete; // No move assignment operator.
 
     template <class T>
-    T& get(const std::size_t index) const {
-        return ((T*) data)[index];
-    }
+        T& get(const std::size_t index) const {
+            return ((T*) data)[index];
+        }
 
     char* slot() {
         char* front_slot = front;
@@ -204,65 +202,71 @@ struct Block {
 Block* block = nullptr;
 
 void render(unsigned char* pixel, int i, int j) {
+    if (!i && !j) printf("\nNEW FRAME\n");
+
     const float screen_x = i / (float) WIDTH - 0.5;
     const float screen_y = j / (float) HEIGHT - 0.5;
 
     const float time = t / 60.0F;
 
     // start traverse on root voxel
-    VoxelStack stack(20);
-    stack.push(&block->get<Voxel>(0), 0);
     AABB box(Vector(-1, -1, -1), 2);
     Ray ray(Vector(0,0,0), Vector(sin(time)+screen_x, cos(time)+screen_y, cos(time)).normalized());
-    stack.peek_oct() = box.get_octant(ray.origin);
+    VoxelStack stack(20);
+    stack.push(&block->get<Voxel>(0),
+                box.get_octant(ray.origin),
+                box.corner
+                );
     float distance = 0;
-
-    pixel[0] = 0;
-    pixel[1] = 0;
-    pixel[2] = 0;
-
 
     while (true) {
         if (stack.empty()) {
-            pixel[1] = 0xaf;
-            pixel[2] = 0xaf;
+            // XXX EMPTY STACK - YELLOW
+            pixel[0] = 0xff;
+            pixel[1] = 0xff;
             break;
         }
 
-        uint8_t oct = stack.peek_oct();
-        bool valid = (stack.peek()->valid >> oct) & 1;
-        bool leaf = (stack.peek()->leaf >> oct) & 1;
+        uint8_t oct = stack.peek().octant;
+        bool valid = (stack.peek().voxel->valid >> oct) & 1;
+        bool leaf = (stack.peek().voxel->leaf >> oct) & 1;
         if (leaf) {
             /* Ray origin is inside leaf voxel, render leaf. */
-            Leaf* leaf = &block->get<Leaf>(stack.peek()->child + oct);
+            Leaf* leaf = &block->get<Leaf>(stack.peek().voxel->child + oct);
+            // XXX LEAF - RED
             pixel[0] = 0xff - distance;
+            pixel[1] = 0x00;
+            pixel[2] = 0x00;
             break;
-        } else if (valid) {
-            /* Go a level deeper. */
-            stack.push(&block->get<Voxel>(stack.peek()->child + oct), oct);
-            box.size *= 0.5;
-            pixel[0] += 0x20;
-        } else {
+        } 
+
+        /* Go a level deeper. */
+
+        // Adjust inferior corner
+        box.size *= 0.5;
+        if (oct & 4) box.corner.x += box.size;
+        if (oct & 2) box.corner.y += box.size;
+        if (oct & 1) box.corner.z += box.size;
+
+        stack.push(&block->get<Voxel>(stack.peek().voxel->child + oct),
+                   oct,
+                   box.corner);
+
+        // XXX EVERY MARCH - SLIGHTLY REDDER
+        pixel[0] += 0x10;
+
+        if (!valid) {
             /* Ray origin is in invalid voxel, cast ray until it hits next
              * voxel. 
              */
-            AABB child(box.corner, box.size * 0.5);
-
-            if (oct & 4) child.corner.x += child.size;
-            if (oct & 2) child.corner.y += child.size;
-            if (oct & 1) child.corner.z += child.size;
-
-            Vector upper(child.corner.x + child.size,
-                         child.corner.y + child.size,
-                         child.corner.z + child.size);
-
+            StackEntry& child = stack.peek();
             uint8_t mask = ray.octant_mask();
             Vector mirror_direction = ray.direction.mirror(mask);
             Vector mirror_origin = ray.origin.mirror(mask);
             Vector mirror_corner = child.corner.mirror(mask);
-            float tx = (box.corner.x - mirror_origin.x) / mirror_direction.x;
-            float ty = (box.corner.y - mirror_origin.y) / mirror_direction.y;
-            float tz = (box.corner.z - mirror_origin.z) / mirror_direction.z;
+            float tx = (child.corner.x - mirror_origin.x) / mirror_direction.x;
+            float ty = (child.corner.y - mirror_origin.y) / mirror_direction.y;
+            float tz = (child.corner.z - mirror_origin.z) / mirror_direction.z;
             float t = std::numeric_limits<float>::infinity();
 
             /* Detect which face hit. */
@@ -270,68 +274,61 @@ void render(unsigned char* pixel, int i, int j) {
             bool direction;
 
             /* t is the smallest positive value of {tx, ty, tz} */
-            if (tx >= 0) {
+            if (tx > 0) {
                 t = tx;
                 hit_face = 4;
             }
-            if (ty >= 0 && ty < t) {
+            if (ty > 0 && ty < t) {
                 t = ty;
                 hit_face = 2;
             }
-            if (tz >= 0 && tz < t) {
+            if (tz > 0 && tz < t) {
                 t = tz;
                 hit_face = 1;
             }
             if (!hit_face) {
-                pixel[2] = 0x8f;
-                printf("BLUE\n");
-                printf("Mask:          %d\n", mask);
-                printf("Box corner:    ");
-                printv(box.corner);
-                printf("Mirror corner: ");
-                printv(mirror_corner);
-                printf("Box size:      %f\n", box.size);
-                printf("Ray direction: ");
-                printv(ray.direction);
-                printf("Mirror directi:");
-                printv(mirror_direction);
-                printf("Ray origin:    ");
-                printv(ray.origin);
-                printf("Face hit:       %d\n", hit_face);
-                printf("tx, ty, tz, t:  (%f, %f, %f) -> %f\n", tx, ty, tz, t);
-                printf("\n");
-                return;
-            } else pixel[2] = hit_face * 10;
+                // XXX NO FACE HIT - BLUER
+                pixel[2] = 0xff;
+                //printf("BLUE\n");
+            } //else pixel[2] = hit_face * 10;
 
             if (!i && !j){
-                printf("Mask:          %d\n", mask);
-                printf("Box corner:    ");
-                printv(box.corner);
-                printf("Mirror corner: ");
-                printv(mirror_corner);
-                printf("Box size:      %f\n", box.size);
-                printf("Ray direction: ");
-                printv(ray.direction);
-                printf("Mirror directi:");
-                printv(mirror_direction);
-                printf("Ray origin:    ");
-                printv(ray.origin);
-                printf("Face hit:       %d\n", hit_face);
-                printf("tx, ty, tz, t:  (%f, %f, %f) -> %f\n", tx, ty, tz, t);
-                printf("\n");
+            printf("Pixel (x, y):  (%d, %d)\n", i, j);
+            printf("Mask:          %d\n", mask);
+            printf("Child box corn:");
+            printv(child.corner);
+            printf("Mirror corner: ");
+            printv(mirror_corner);
+            printf("Box size:      %f\n", box.size);
+            printf("Ray direction: ");
+            printv(ray.direction);
+            printf("Mirror directi:");
+            printv(mirror_direction);
+            printf("Ray origin:    ");
+            printv(ray.origin);
+            printf("Face hit:       %d\n", hit_face);
+            printf("tx, ty, tz, t:  (%f, %f, %f) -> %f\n", tx, ty, tz, t);
+            printf("\n");
             }
+
+            if (!hit_face) return;
 
             /* Ray will start next step at the point of this intersection */
             t += std::numeric_limits<float>::epsilon();
             ray.origin = Vector(
-                t * ray.direction.x + ray.origin.x,
-                t * ray.direction.y + ray.origin.y,
-                t * ray.direction.z + ray.origin.z
-            ).mirror(mask);
+                    t * ray.direction.x + ray.origin.x,
+                    t * ray.direction.y + ray.origin.y,
+                    t * ray.direction.z + ray.origin.z
+                    );
 
-            while (hit_face & (stack.peek_oct() ^ mask)) {
+            // Start with parent
+            stack.pop();
+            box.size *= 2;
+
+            while (hit_face & (stack.peek().octant ^ mask)) {
                 if (stack.empty()) {
                     /* Ray is outside root octree. */
+                    // XXX EMPTY STACK - DARK GREEN
                     pixel[1] = 0x8f;
                     return;
                 }
@@ -349,9 +346,8 @@ void render(unsigned char* pixel, int i, int j) {
              * Transfer to sibling voxel, changing on the axis of the face
              * that was hit.
              */
-            stack.peek_oct() = (stack.peek_oct() ^ hit_face);
+            stack.peek().octant = (stack.peek().octant ^ hit_face);
         }
-
     }
 }
 
@@ -373,22 +369,22 @@ void renderScene() {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 
     glTexImage2D (
-        GL_TEXTURE_2D,
-        0,
-        GL_RGB,
-        WIDTH,
-        HEIGHT,
-        0,
-        GL_RGB,
-        GL_UNSIGNED_BYTE,
-        texture
-    );
+            GL_TEXTURE_2D,
+            0,
+            GL_RGB,
+            WIDTH,
+            HEIGHT,
+            0,
+            GL_RGB,
+            GL_UNSIGNED_BYTE,
+            texture
+            );
 
     glBegin(GL_QUADS);
-        glTexCoord2f(0.0f, 0.0f); glVertex2f(-1.0, -1.0);
-        glTexCoord2f(1.0f, 0.0f); glVertex2f( 1.0, -1.0);
-        glTexCoord2f(1.0f, 1.0f); glVertex2f( 1.0,  1.0);
-        glTexCoord2f(0.0f, 1.0f); glVertex2f(-1.0,  1.0);
+    glTexCoord2f(0.0f, 0.0f); glVertex2f(-1.0, -1.0);
+    glTexCoord2f(1.0f, 0.0f); glVertex2f( 1.0, -1.0);
+    glTexCoord2f(1.0f, 1.0f); glVertex2f( 1.0,  1.0);
+    glTexCoord2f(0.0f, 1.0f); glVertex2f(-1.0,  1.0);
     glEnd();
 
     glFlush();
